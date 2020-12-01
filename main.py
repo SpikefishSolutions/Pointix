f9b26f8ddd86166158a9385d47180fe5abc36b58
Devin-Marks <dmarks2706@floridapoly.edu>
2020-12-01 17:27:16 -0500

    Added License Disclaimer

--- main.py
+++ main.py
@@ -3,11 +3,21 @@
 ############# Spikefish Solutions Inc. ####################
 ###########################################################
 
-###########################################################
-# With a group of Check Point MDSs in Zabbix, this script #
-### will query all of those MDSs for all objects under ####
-###### each MDS and automatically add them to Zabbix ######
-###########################################################
+# This file is part of Pointix
+# Copyright (C) 2020 Spikefish Solutions
+
+# Pointix is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Pointix is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Pointix.  If not, see <https://www.gnu.org/licenses/>.
 
 import sys
 import getopt
